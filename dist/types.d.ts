d5aef1571e6fd6060b6fc786df0c31643c2e9ee1
tim <timmyg13@gmail.com>
2021-02-25 20:51:16 -0500

    add dist with navigateWithoutPopup param

---
+++ dist/types.d.ts
@@ -0,0 +1,26 @@
+declare namespace Types {
+    interface ConnectOptions {
+        authId?: string;
+        setupId?: string;
+        params?: any;
+        navigateWithoutPopup?: boolean;
+    }
+    interface ConnectSuccess {
+        authId: string;
+    }
+    interface ConnectError extends Error {
+    }
+    interface IntegrationOptions {
+        authId?: string;
+        setupId?: string;
+    }
+    type RequestHeaders = Record<string, string | number | undefined>;
+    type RequestMethod = 'GET' | 'DELETE' | 'HEAD' | 'OPTIONS' | 'POST' | 'PUT' | 'PATCH';
+    type RequestQueryString = Record<string, string | number>;
+    interface RequestParameters {
+        headers?: RequestHeaders;
+        query?: RequestQueryString;
+        body?: any;
+    }
+}
+export default Types;
